ecf8f608631de2617ec837cc7b306fa2006ba43a
YOOKEUNCHANG <dinermint@dongguk.edu>
2018-05-13 01:06:12 +0900

    login ui fix by 유근창
    로그인 관련 ui 변경

---
+++ app/src/main/java/com/example/alex/dg_chatbot/UI/Login/SignInActivity.java
@@ -0,0 +1,89 @@
+package com.example.alex.dg_chatbot.UI.Login;
+
+import android.content.Intent;
+import android.provider.DocumentsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.alex.dg_chatbot.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class SignInActivity extends RootLogin {
+    private EditText etUserId;
+    private EditText etUserPwd;
+    private Button btEmailLogin;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        etUserId = findViewById(R.id.etUserId);
+        etUserPwd = findViewById(R.id.etUserPwd);
+        btEmailLogin = findViewById(R.id.btEmailLogin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btEmailLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emailSignIn(etUserId.getText().toString(), etUserPwd.getText().toString());
+            }
+        });
+    }
+
+    //유효성 검사
+    public boolean isVaild() {
+        String email = this.etUserId.getText().toString();
+        String pwd = this.etUserPwd.getText().toString();
+        if(TextUtils.isEmpty(email)) {
+            this.etUserId.setError("이메일을 입력하세요");
+            return false;
+        }
+        if(TextUtils.isEmpty(pwd)) {
+            this.etUserPwd.setError("비밀번호를 입력하세요");
+            return false;
+        }
+        return true;
+    }
+
+    //이메일 비번으로 로그인
+    //유효성 검사 후 로그인 할 지 안 할지 결
+    public void emailSignIn(String email, String pwd){
+        showPD();
+
+        firebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignInActivity.this, "이메일 로그인 성공", Toast.LENGTH_LONG).show();
+                    startSelectActivity();
+                }else{
+                    Toast.makeText(SignInActivity.this, "이메일 로그인 실패", Toast.LENGTH_LONG).show();
+                }
+                stopPD();
+            }
+        });
+
+        if(!isVaild()) return;
+
+
+    }
+    public void startSelectActivity(){
+        Intent intent = new Intent(this, SelectFavoriteActivity.class);
+        startActivity(intent);
+    }
+
+}
